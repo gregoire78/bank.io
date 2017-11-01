573d61ffba8ea21b6dc9df3ebe1d7793d62f29fc
Grégoire <greg.joncour@gmail.com>
2017-11-01 20:36:06 +0100

    partie user

--- API/controllers/Users.js
+++ API/controllers/Users.js
@@ -14,28 +14,22 @@ module.exports = Users = {
 
 		var user = matchedData(req);
 
-		User.findOne({'email': user.email}, function (err, userr) {
-			if(userr === null || user.email !== userr.email){
-				bcrypt.hash(user.password, 10).then(function (hash) {
+		bcrypt.hash(user.password, 10).then(function (hash) {
 
-					var userModel = new User();
+			var userModel = new User();
 
-					userModel.firstName = user.firstname;
-					userModel.lastName = user.lastname;
-					userModel.email = user.email;
-					userModel.password = hash;
-					userModel.address.city = user.address.city;
-					userModel.address.street = user.address.street;
-					userModel.address.postal_code = user.address.postalCode;
+			userModel.firstName = user.firstname;
+			userModel.lastName = user.lastname;
+			userModel.email = user.email;
+			userModel.password = hash;
+			userModel.address.city = user.address.city;
+			userModel.address.street = user.address.street;
+			userModel.address.postal_code = user.address.postalCode;
 
-					userModel.save(function (err) {
-						if (err) return next(err);
-						res.json('Successfully register a new user');
-					});
-				});
-			}else {
-				res.json({error: 'email déjà utlisé'})
-			}
+			userModel.save(function (err) {
+				if (err) return next(err);
+				res.json('Successfully register a new user');
+			});
 		});
 	}
 };
